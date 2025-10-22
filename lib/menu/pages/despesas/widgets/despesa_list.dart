4ca156fd0fabf14f67ea03d7b626a6a369179ea2
Natan Valente <natan0404@hotmail.com>
2025-10-22 18:00:01 -0300

    Iniciado desenvolvimento do Crud de despesa.

---
+++ lib/menu/pages/despesas/widgets/despesa_list.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class DespesaList extends StatefulWidget {
+  const DespesaList({super.key});
+
+  @override
+  State<DespesaList> createState() => _DespesaListState();
+}
+
+class _DespesaListState extends State<DespesaList> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
